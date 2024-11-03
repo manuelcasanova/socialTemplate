270a481a62d8865a7c1e8d716ae9e1334870af79
Manuel Casanova <manucasanova@hotmail.com>
2024-11-03 05:45:35 -0400

    Basic layout with navigation bar, body, footer, responsive design, hide and show navigation bar

--- CLIENT/src/App.js
+++ CLIENT/src/App.js
@@ -1,11 +1,36 @@
 import './css/App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
   return (
-    <div className="App">
-    
+    <div className="app">
+      <button className="hamburger" onClick={toggleNav}>
+      {isNavOpen ? '✖' : '☰'}
+      </button>
+      <header className={`navbar ${isNavOpen ? 'open' : ''}`}>
+        <nav className={isNavOpen ? 'nav-open' : ''}>
+NAVIGATION BAR
+        </nav>
+      </header>
+      <main className={`body ${isNavOpen ? 'squeezed' : ''}`}>
+BODY
+      </main>
+      <footer className={`footer ${isNavOpen ? 'squeezed' : ''}`}>
+FOOTER
+      </footer>
     </div>
   );
 }
+
+
+
 
 export default App;
