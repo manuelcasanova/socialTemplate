2c4cf9ea55cc98711068bac58b4f6ce6444b2f2a
Manuel Casanova <manucasanova@hotmail.com>
2024-12-28 23:18:36 -0800

    Modify node cron, once a day check for subscription renewal date and is active status and delete user_roles if expired or inactive

--- SERVER/routes/subscriptionScheduler.js
+++ SERVER/routes/subscriptionScheduler.js
@@ -1,16 +1,28 @@
 const cron = require('node-cron');
-const pool= require('../config/db'); 
+const pool = require('../config/db');
 
 const scheduleSubscriptionUpdates = () => {
   cron.schedule('0 0 * * *', async () => { // Runs daily at midnight
 
     try {
-      const result = await pool.query(`
+      const updateSubscriptions = await pool.query(`
         UPDATE subscriptions
         SET is_active = false
         WHERE renewal_due_date < NOW() AND is_active = true;
       `);
-      // console.log(`${result.rowCount} subscriptions updated`);
+
+      // Remove the 'User_subscribed' role (role_id = 2) from the user_roles table for users with expired subscriptions
+      const removeUserSubscribedRole = await pool.query(`
+         DELETE FROM user_roles
+         WHERE role_id = 2
+         AND user_id IN (
+         SELECT user_id
+         FROM subscriptions
+         WHERE is_active = false AND renewal_due_date < NOW()
+        );
+      `);
+      console.log(`${updateSubscriptions.rowCount} subscriptions updated at ${Date.now()}`);
+      console.log(`${removeUserSubscribedRole.rowCount} roles removed at ${Date.now()}`);
     } catch (error) {
       console.error('Error updating subscriptions:', error);
     }
