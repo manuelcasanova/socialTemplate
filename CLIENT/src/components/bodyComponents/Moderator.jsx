78eb86e5f1e38e93d4640f630ac0c4102fb53e9c
Manuel Casanova <manucasanova@hotmail.com>
2025-01-29 22:02:46 -0800

    Show Admin Users list once fetchUsersAndRoles is done

--- CLIENT/src/components/bodyComponents/Moderator.jsx
+++ CLIENT/src/components/bodyComponents/Moderator.jsx
@@ -4,8 +4,20 @@ export default function Moderator({ isNavOpen }) {
 
   return (
     <div className={`${isNavOpen ? 'body-squeezed' : 'body'}`}>
-      <h2>All moderator users have access to this page</h2>
+      <div className="centered-container">
+        <h2>This page is private and accessible only to Moderators.</h2>
+
+        <div className="interaction-box">
+          <p>The app is designed to include moderators, who could serve as an intermediary between regular users and administrators. For example, in a social section where users post comments, moderators could oversee conversations and remove any comments that violate the guidelines. While they don’t have full admin privileges, they would be able to intervene in a limited capacity to maintain order.</p>
+
+        </div>
+      </div>
     </div>
-    
+
   )
 }
+
+
+
+
+
--- CLIENT/src/components/bodyComponents/users/AdminUsers.jsx
+++ CLIENT/src/components/bodyComponents/users/AdminUsers.jsx
@@ -30,6 +30,7 @@ export default function AdminUsers({ isNavOpen }) {
 
   useEffect(() => {
     const fetchUsersAndRoles = async () => {
+      setIsLoading(true)
       try {
         // Fetch users and roles
         const [usersResponse, rolesResponse] = await Promise.all([
@@ -43,6 +44,8 @@ export default function AdminUsers({ isNavOpen }) {
       } catch (err) {
         setError(`Failed to fetch data: ${err.response.data.error}`);
         console.error(err);
+      } finally {
+        setIsLoading(false);
       }
     };
 
@@ -135,127 +138,133 @@ export default function AdminUsers({ isNavOpen }) {
           setFilters={setFilters}
           setExpandedUserId={setExpandedUserId}
         />
-        <div className="users-container">
-          {users.length > 0 ? (
-            users.map((user) =>
-              expandedUserId === null || expandedUserId === user.user_id ? (
-                <div className="user-row" key={user.user_id}>
-                  <div className="user-info">
-                    {expandedUserId !== user.user_id && <p>
-                      {user.username.startsWith('inactive') ? 'Inactive User' : user.username}
-                    </p>}
-                    <button onClick={() => handleViewMore(user.user_id)}
-                      className={expandedUserId === user.user_id ? "user-info-expanded" : ""}
-                    >
-                      {expandedUserId === user.user_id ? "-" : "+"}
-                    </button>
-                  </div>
-
-                  {expandedUserId === user.user_id && (
-
-                    <div className="user-details">
-                      <div className="admin-profile-image-container">
-                        <img
-                          src={`${BACKEND}/media/profile_pictures/${user.user_id}/profilePicture.jpg`}
-                          alt={user.username.startsWith('inactive') ? 'Inactive User profile image' : `${user.username}'s profile image`}
-
-                          className="admin-profile-image"
-                          onError={(e) => {
-                            e.target.style.display = "none";  // Hide broken image
-                            // You could also show the fallback icon in the next line if needed
-                            e.target.nextSibling.style.display = "inline-block";  // Show icon
-                          }}
-                        />
-                        {/* Display FontAwesome icon if image is not found */}
-                        <FontAwesomeIcon
-                          icon={faUser}
-                          className="admin-profile-image"
-                          style={{ display: 'none' }}  // Initially hidden
-                        />
-                      </div>
-                      <p>
-                        <strong>Username:</strong> {user.username.startsWith('inactive') ? 'Inactive User' : user.username}
-                      </p>
-                      <p><strong>E-mail:</strong> {user.email}</p>
-                      <p><strong>Verified:</strong> {user.is_verified ? "Yes" : "No"}</p>
-                      <p><strong>Active:</strong> {user.is_active ? "Yes" : "No"}</p>
-                      <h4>Roles</h4>
-                      <ul>
-                        {roles.map((role, index) => (
-                          <li key={index}>
-                            <input
-                              type="checkbox"
-                              className="checkbox"
-                              checked={user.roles.includes(role)}
-                              onChange={(e) => {
-                                setError("");  // Clear any previous error when the checkbox is clicked
-                                handleRoleChange(user, role, e.target.checked); // Pass the full user object here
-                              }}
-                            />
-                            {role}
-                          </li>
-                        ))}
-                      </ul>
-                      <h4>Last login</h4>
-                      {user.login_history.length > 0 ? (
-                        <ul>
-                          <li>
-                            {(() => {
-                              const lastLogin = new Date(user.login_history[user.login_history.length - 1]);
-                              return lastLogin.toLocaleString('en-US', {
-                                weekday: 'long',  // Full name of the weekday (e.g., "Monday")
-                                month: 'long',    // Full name of the month (e.g., "December")
-                                day: 'numeric',   // Day of the month (e.g., "9")
-                                year: 'numeric',  // Full year (e.g., "2024")
-                                hour: '2-digit',  // Hour in 12-hour format (e.g., "5")
-                                minute: '2-digit',// Minutes (e.g., "42")
-                                hour12: true      // Use 12-hour clock with AM/PM
-                              });
-                            })()}
-                          </li>
-
-                        </ul>
-                      ) : (
-                        <p>No login history available</p>
-                      )}
-
-
-
-                      {
-                        !showConfirmDelete && !user.email.startsWith('deleted-') && (
-                          <div className="delete-user">
-                            <button
-                              className="button-red"
-                              onClick={handleShowDelete}
-                            >Delete user</button>
-                          </div>
-                        )
-                      }
-
-                      {
-                        showConfirmDelete && !user.email.startsWith('deleted-') && (
-                          <div className="delete-confirmation">
-                            <p>Are you sure you want to delete this user? This action is permanent and cannot be undone.</p>
-                            <button className="button-white" onClick={handleShowDelete}>x</button>
-                            <button
-                              className="button-red"
-                              disabled={isLoading}
-                              onClick={() => handleDeleteUser(user.user_id, loggedInUser)}
-                            >{isLoading ? <LoadingSpinner /> : 'Confirm delete'}</button>
-
-                          </div>
-                        )
-                      }
-
+        {isLoading ? (
+          <LoadingSpinner />
+        ) :
+          <div className="users-container">
+            {users.length > 0 ? (
+              users.map((user) =>
+                expandedUserId === null || expandedUserId === user.user_id ? (
+                  <div className="user-row" key={user.user_id}>
+                    <div className="user-info">
+                      {expandedUserId !== user.user_id && <p>
+                        {user.username.startsWith('inactive') ? 'Inactive User' : user.username}
+                      </p>}
+                      <button onClick={() => handleViewMore(user.user_id)}
+                        className={expandedUserId === user.user_id ? "user-info-expanded" : ""}
+                      >
+                        {expandedUserId === user.user_id ? "-" : "+"}
+                      </button>
                     </div>
-                  )}
-                </div>
-              ) : null
-            )
-          ) : (
-            <p>No users found</p>
-          )}
-        </div>
+
+                    {expandedUserId === user.user_id && (
+
+                      <div className="user-details">
+                        <div className="admin-profile-image-container">
+                          <img
+                            src={`${BACKEND}/media/profile_pictures/${user.user_id}/profilePicture.jpg`}
+                            alt={user.username.startsWith('inactive') ? 'Inactive User profile image' : `${user.username}'s profile image`}
+
+                            className="admin-profile-image"
+                            onError={(e) => {
+                              e.target.style.display = "none";  // Hide broken image
+                              // You could also show the fallback icon in the next line if needed
+                              e.target.nextSibling.style.display = "inline-block";  // Show icon
+                            }}
+                          />
+                          {/* Display FontAwesome icon if image is not found */}
+                          <FontAwesomeIcon
+                            icon={faUser}
+                            className="admin-profile-image"
+                            style={{ display: 'none' }}  // Initially hidden
+                          />
+                        </div>
+                        <p>
+                          <strong>Username:</strong> {user.username.startsWith('inactive') ? 'Inactive User' : user.username}
+                        </p>
+                        <p><strong>E-mail:</strong> {user.email}</p>
+                        <p><strong>Verified:</strong> {user.is_verified ? "Yes" : "No"}</p>
+                        <p><strong>Active:</strong> {user.is_active ? "Yes" : "No"}</p>
+                        <h4>Roles</h4>
+                        <ul>
+                          {roles.map((role, index) => (
+                            <li key={index}>
+                              <input
+                                type="checkbox"
+                                className="checkbox"
+                                checked={user.roles.includes(role)}
+                                onChange={(e) => {
+                                  setError("");  // Clear any previous error when the checkbox is clicked
+                                  handleRoleChange(user, role, e.target.checked); // Pass the full user object here
+                                }}
+                              />
+                              {role}
+                            </li>
+                          ))}
+                        </ul>
+                        <h4>Last login</h4>
+                        {user.login_history.length > 0 ? (
+                          <ul>
+                            <li>
+                              {(() => {
+                                const lastLogin = new Date(user.login_history[user.login_history.length - 1]);
+                                return lastLogin.toLocaleString('en-US', {
+                                  weekday: 'long',  // Full name of the weekday (e.g., "Monday")
+                                  month: 'long',    // Full name of the month (e.g., "December")
+                                  day: 'numeric',   // Day of the month (e.g., "9")
+                                  year: 'numeric',  // Full year (e.g., "2024")
+                                  hour: '2-digit',  // Hour in 12-hour format (e.g., "5")
+                                  minute: '2-digit',// Minutes (e.g., "42")
+                                  hour12: true      // Use 12-hour clock with AM/PM
+                                });
+                              })()}
+                            </li>
+
+                          </ul>
+                        ) : (
+                          <p>No login history available</p>
+                        )}
+
+
+
+                        {
+                          !showConfirmDelete && !user.email.startsWith('deleted-') && (
+                            <div className="delete-user">
+                              <button
+                                className="button-red"
+                                onClick={handleShowDelete}
+                              >Delete user</button>
+                            </div>
+                          )
+                        }
+
+                        {
+                          showConfirmDelete && !user.email.startsWith('deleted-') && (
+                            <div className="delete-confirmation">
+                              <p>Are you sure you want to delete this user? This action is permanent and cannot be undone.</p>
+                              <button className="button-white" onClick={handleShowDelete}>x</button>
+                              <button
+                                className="button-red"
+                                disabled={isLoading}
+                                onClick={() => handleDeleteUser(user.user_id, loggedInUser)}
+                              >{isLoading ? <LoadingSpinner /> : 'Confirm delete'}</button>
+
+                            </div>
+                          )
+                        }
+
+                      </div>
+                    )}
+                  </div>
+                ) : null
+              )
+            ) : (
+              <p>No users found</p>
+            )}
+          </div>
+        }
+
+
       </div>
     </div>
   );
