463b0be24677fbeb2f185dbf63362dbfa494abe6
Manuel Casanova <manuelcasanova@Manuels-MacBook-Air-2.local>
2025-02-12 15:46:48 -0800

    Show faUser if no Profile Image

--- CLIENT/src/components/bodyComponents/socialComponents/SocialAllUsers.jsx
+++ CLIENT/src/components/bodyComponents/socialComponents/SocialAllUsers.jsx
@@ -10,12 +10,25 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const BACKEND = process.env.REACT_APP_BACKEND_URL;
 
+// Function to check if profile picture exists for each user
+const profilePictureExists = async (userId) => {
+  const imageUrl = `${BACKEND}/media/profile_pictures/${userId}/profilePicture.jpg`;
+  try {
+    const response = await fetch(imageUrl, { method: 'HEAD' });
+    return response.ok;
+  } catch (error) {
+    console.error("Error checking image existence:", error);
+    return false;
+  }
+};
+
 export default function SocialAllUsers({ isNavOpen }) {
   const axiosPrivate = useAxiosPrivate();
   const [users, setUsers] = useState([]);
   const [error, setError] = useState(null);
   const [filters, setFilters] = useState({});
   const [isLoading, setIsLoading] = useState(false);
+  const [imageExistsMap, setImageExistsMap] = useState({});
 
   // Reset the error message whenever filters change
   useEffect(() => {
@@ -43,6 +56,21 @@ export default function SocialAllUsers({ isNavOpen }) {
     fetchUsers();
   }, [axiosPrivate, filters]);
 
+    // Check if profile picture exists for each user and store the result
+    useEffect(() => {
+      const checkImages = async () => {
+        const result = {};
+        for (const user of users) {
+          result[user.user_id] = await profilePictureExists(user.user_id);
+        }
+        setImageExistsMap(result);
+      };
+  
+      if (users.length > 0) {
+        checkImages();
+      }
+    }, [users]);
+
   return (
     <div className={`${isNavOpen ? 'body-squeezed' : 'body'}`}>
       <div className="admin-users">
@@ -56,10 +84,16 @@ export default function SocialAllUsers({ isNavOpen }) {
 
                 <div className="user-row-social" key={user.user_id}>
                   <div className="user-info">
-                    <img
-                      src={`${BACKEND}/media/profile_pictures/${user.user_id}/profilePicture.jpg`}
-                      alt="Profile"
-                    />
+                  {imageExistsMap[user.user_id] ? (
+                      <img
+                        src={`${BACKEND}/media/profile_pictures/${user.user_id}/profilePicture.jpg`}
+                        alt="Profile"
+                      />
+                    ) : (
+                      
+                      <FontAwesomeIcon icon={faUser} size="3x" style={{marginRight: '20px'}}  />
+                    
+                    )}
                     <p>
                       {user.username.startsWith('inactive') ? 'Inactive User' : user.username}
                     </p>
