1cd9edbe5468eb6e902aa776b7687830f594824c
Manuel Casanova <manucasanova@hotmail.com>
2025-06-30 11:51:35 -0700

    Mofidy query to fetch roles ordered by number

--- SERVER/controllers/customRolesController.js
+++ SERVER/controllers/customRolesController.js
@@ -3,7 +3,7 @@ const pool = require('../config/db');
 // Function to get all custom roles (is_system_role = false)
 const getAllCustomRoles = async (req, res) => {
   try {
-    const query = 'SELECT * FROM roles WHERE is_system_role = $1 ORDER BY role_name';
+    const query = `SELECT * FROM roles WHERE is_system_role = $1 ORDER BY regexp_replace(role_name, '\\D', '', 'g')::int `;
     const queryParams = [false];
 
     const result = await pool.query(query, queryParams);
